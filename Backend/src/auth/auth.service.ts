8421dc67faf880615a7c427af141f06403825d23
EmmanoelCoutinho <emmanoelcesar967@gmail.com>
2022-08-06 03:05:15 -0300

    criação e envio para o front do jwt

--- Backend/src/auth/auth.service.ts
+++ Backend/src/auth/auth.service.ts
@@ -1,10 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcrypt';
+import { User } from 'src/user/entities/user.entity';
+import { UserPayload } from './TSmodels/UserPayload';
+import { JwtService } from '@nestjs/jwt';
+import { UserToken } from './TSmodels/UserToken';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  login(user: User): UserToken {
+    //transforma o user em JWT
+    const payload: UserPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+    };
+
+    const jwtToken = this.jwtService.sign(payload);
+
+    return {
+      access_token: jwtToken,
+    };
+  }
 
   async validateUser(email: string, password: string): Promise<any> {
     const user: any = await this.userService.findByEmail(email);
