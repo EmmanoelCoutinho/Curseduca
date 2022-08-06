a01d3a00ff625240cb349025e486dd9dff388270
EmmanoelCoutinho <emmanoelcesar967@gmail.com>
2022-08-06 02:26:15 -0300

    validação do usuário e senha no backend concluida

--- Backend/src/auth/auth.service.ts
+++ Backend/src/auth/auth.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user: any = await this.userService.findByEmail(email);
+
+    if (user) {
+      const isPasswordMatching = await bcrypt.compare(password, user.password);
+      if (isPasswordMatching) {
+        return { ...user, password: undefined };
+      }
+    }
+    throw new Error('Invalid credentials');
+  }
+}
